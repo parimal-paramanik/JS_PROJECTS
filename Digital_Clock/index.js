e417a4ae1bff9c6a036ced92831a811920ca2760
unknown <smartdesk2015@gmail.com>
2023-09-20 01:29:07 +0530

    digital clock

--- Digital_Clock/index.js
+++ Digital_Clock/index.js
@@ -8,13 +8,16 @@ function showTime() {
     var H = D.getHours();
     var M = D.getMinutes();
     var S = D.getSeconds();
-
     
     var session = 'AM'
+    if(H > 12){
+        H = H-12;
+    }
+    session = (H >=12) ? "PM" :session
     H = (H < 10) ? ("0" + H) : H
     M = (M < 10) ? ("0" + M) : M
     S = (S < 10) ? ("0" + S) : S
-    session = (H >=12) ? "PM" :session
+
 
     var time = H + " : " + M + " : " + S + "  " + " " + session;
     document.getElementsByTagName("h1")[0].innerText = time
